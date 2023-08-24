14f2817cd800bb3b2498893b1bb44246937c0ff3
Alberto Rivas <albertomasr@hotmail.com>
2023-08-23 22:37:28 -0700

    Marriage proposal scene

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as BABYLON from '@babylonjs/core';
+import earcut from 'earcut';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.getElementById('renderCanvas');
 
-setupCounter(document.querySelector('#counter'))
+const engine = new BABYLON.Engine(canvas, true);
+
+const createScene = async () => {
+    const scene = new BABYLON.Scene(engine);
+
+    const camera = new BABYLON.ArcRotateCamera('camera', -Math.PI / 2, Math.PI / 2.2, 40, new BABYLON.Vector3(0, 0, 0), scene);
+
+    camera.attachControl(canvas, true);
+
+    const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 1, 0), scene);
+
+    const fontData = await (await fetch('/Noto Sans Medium_Regular.json')).json();
+    const textoPropuesta = BABYLON.MeshBuilder.CreateText('Propuesta', '¿Te quieres casar conmigo?', fontData, {
+        size: 2,
+        depth: 0.5,
+    }, scene, earcut);
+
+    return scene;
+};
+
+const scene = await createScene();
+
+engine.runRenderLoop(() => {
+    scene.render();
+});
+
+window.addEventListener('resize', () => {
+    engine.resize();
+});
